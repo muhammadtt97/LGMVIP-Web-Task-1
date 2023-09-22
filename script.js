07e017a45a8b9633f4aa9715710309bc9452faee
Muhammad Al-ameen <muhammadtt97@gmail.com>
2023-09-22 16:44:29 +0100

    updated

---
+++ script.js
@@ -0,0 +1,18 @@
+const taskInput = document.getElementById("task");
+const taskList = document.getElementById("taskList");
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `<span>${taskText}</span><button class="delete-button" onclick="deleteTask(this)">Delete</button>`;
+        taskList.appendChild(listItem);
+        taskInput.value = "";
+    }
+}
+
+function deleteTask(button) {
+    const listItem = button.parentElement;
+    taskList.removeChild(listItem);
+}
